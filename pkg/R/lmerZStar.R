c1196e4eb36d6979fdeff602971744eee99c5c20
dmbates <dmbates@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-03-17 21:52:28 +0000

    Initial version of lmer_Zstar; updated dependencies
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pedigreemm@5 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/lmerZStar.R
@@ -0,0 +1,29 @@
+lmer_ZStar <-
+    function(formula, data, family = NULL, pre = NULL, method = c("REML", "ML"),
+             control = list(), start = NULL, verbose = FALSE, subset,
+             weights, na.action, offset, contrasts = NULL,
+             model = TRUE, x = TRUE, ...)
+{
+    mc <- match.call()
+    if (is.null(pre)) {
+        mc$pre <- NULL
+        mv[[1]] <- as.name("lmer")
+        return(eval(mc))
+    }
+    if (!is.null(family)) {             # call glmer_ZStar
+        mc[[1]] <- as.name("glmer_ZStar")
+        return(eval(mc))
+    }
+    stopifnot(length(formula <- as.formula(formula)) == 3)
+
+    fr <- lme4:::lmerFrames(mc, formula, contrasts) # model frame, X, etc.
+    FL <- lme4:::lmerFactorList(formula, fr$mf, 0L, 0L) # flist, Zt, cnames
+    Y <- as.double(fr$Y)
+
+    if (length(pre) != length(FL))
+        stop(paste("length of argument `pre' must be", length(Ztl)))
+    for (i in seq_along(FL))
+        if (!is.null(pre[[i]]))
+            FL[[i]]$Zt <- FL[[i]]$A <- pre[[i]] %*% FL[[i]]$Zt
+    lme4:::lmer_finalize(mc, fr, ST, start, match.arg(method), verbose)
+}
